4d012adff8fc91fe7e9154f153d58a1ad12e6f62
itolimaesther <itolimaesther@gmail.com>
2021-01-18 06:45:34 +0100

    added delete button

--- index.js
+++ index.js
@@ -35,31 +35,31 @@ document.querySelector("#submit-btn").addEventListener("click", () => {
 
                     
                     
+                    let cards = document.querySelector(".cards")
+                    let normWrapper = document.querySelector(".nomination-wrapper")
+                    let normDivs = document.querySelector(".nominations")
+                    let normDiv = document.createElement("div")
+                    let cardDiv = document.createElement("div");
+                    let movieImageBox = document.createElement("div")
+                    let movieImage = document.createElement("div")
+                    let movieDescription = document.createElement("div")
+                    let movieTitle = document.createElement("h2")
+                    let movieRate = document.createElement("p")
+                    let rateSpan = document.createElement("span")
+                    let rateIcon = document.createElement("i")
+                    let rateNumber = document.createElement("span")
+                    let moviePlot = document.createElement("p")
+                    let movieTopic = document.createElement("p")
+                    let typeSpan = document.createElement("span")
+                    let topicGenre = document.createElement("p")
+                    let genreSpan = document.createElement("span")
+                    let nomBtn = document.createElement("button");
+
                     if (type == "movie" || type == "series") {
                       
-                      let cards = document.querySelector(".cards")
-                      let normWrapper = document.querySelector(".nomination-wrapper")
-                      let normDivs = document.createElement("div")
-                      let normDiv = document.createElement("div")
-                      let cardDiv = document.createElement("div");
-                      let movieImageBox = document.createElement("div")
-                      let movieImage = document.createElement("div")
-                      let movieDescription = document.createElement("div")
-                      let movieTitle = document.createElement("h2")
-                      let movieRate = document.createElement("p")
-                      let rateSpan = document.createElement("span")
-                      let rateIcon = document.createElement("i")
-                      let rateNumber = document.createElement("span")
-                      let moviePlot = document.createElement("p")
-                      let movieTopic = document.createElement("p")
-                      let typeSpan = document.createElement("span")
-                      let topicGenre = document.createElement("p")
-                      let genreSpan = document.createElement("span")
-                      let nomBtn = document.createElement("button");
                         
                         cards.appendChild(cardDiv)
                         cardDiv.appendChild(movieImageBox)
-                        normWrapper.appendChild(normDivs)
                         normDivs.appendChild(normDiv)
                         movieImageBox.appendChild(movieImage)
                         cardDiv.appendChild(movieDescription)
@@ -79,44 +79,69 @@ document.querySelector("#submit-btn").addEventListener("click", () => {
                         cards.classList.add( "cards")
                         cardDiv.classList.add("card");
                         normDivs.classList.add("nominations")
+                        normDiv.classList.add("nomination")
                         movieImageBox.classList.add( "card-image-box")
                         movieImage.classList.add( "card-image")
                         movieDescription.classList.add( "card-description")
                         nomBtn.classList.add("nom-btn")
                         movieTitle.classList.add( "card-title")
-                        movieRate.classList.add( "card-rating")
-                        moviePlot.classList.add( "card-plot")
-                        typeSpan.classList.add( "topic")
-                        genreSpan.classList.add("topic")
-                        rateSpan.classList.add("topic")
-                        rateIcon.classList.add("fas")
-                        rateNumber.classList.add("rating")
+
+                        // movieRate.classList.add( "card-rating")
+                        // moviePlot.classList.add( "card-plot")
+                        // typeSpan.classList.add( "topic")
+                        // genreSpan.classList.add("topic")
+                        // rateSpan.classList.add("topic")
+                        // rateIcon.classList.add("fas")
+                        // rateNumber.classList.add("rating")
 
                         movieImage.innerHTML = `<img alt='movie-image'src=${poster}/>`
                         movieDescription.innerHTML = `<h2>${title}, ${year}</h2>`
                         nomBtn.innerText = "Nominate"
+                      }
+                      else{
+                        console.log(`There is also a ${type} with the name: ${title}`);
+                        document.querySelector("#search-field").value = "";
+                      }
+                      
+                      let deleteBtn = document.createElement("button")
+                      let nomName = document.createElement("p")
+
+                      // function nominate(e){
+
+                      //   let btnParent = e.target.parentNode
+                      //   btnParent.appendChild(movieTitle)
 
 
-                        document.querySelector(".nom-btn").addEventListener("click", () => {
-                          console.log(cardDiv.value)
-                          let deleteBtn = document.createElement("button")
-                          // normDivs.innerHTML = `<div> <p>${title}</p>${deleteBtn}</div>`
-                        let nomName = document.createElement("p")
+                      //   normDiv.appendChild(nomName)
+                      //   normDiv.appendChild(deleteBtn)
 
-                        normWrapper.appendChild(nomName)
-                        // nomName.appendChild(deleteBtn)
+                      //   nomName.innerText = `${title}`
+                      //   deleteBtn.innerText = "Delete"
+
+                      // }
+
+                      // function deleteNomination(e){
+                      //   let btnParent = e.target.parentNode
+                      //   btnParent.removeChild(movieTitle)
+                      // }
+
+                      nomBtn.addEventListener("click", (e) => {
+                        
+                        normDiv.appendChild(nomName)
+                        normDiv.appendChild(deleteBtn)
+
+                        nomName.innerText = `${title}`
                         deleteBtn.innerText = "Delete"
-                        normDiv.innerHTML=`<p>${title}</p>`
+                        deleteBtn.style.backgroundColor = "red"
 
-                          console.log("hello")
-                        })
+                        nomBtn.style.backgroundColor="grey"
+                      })
 
-                    }
-                    else{
-                      console.log(`There is also a ${type} with the name: ${title}`);
-                      document.querySelector("#search-field").value = "";
-                    }
+                      deleteBtn.addEventListener("click", (e)=>{
+                        deleteBtn.parentNode.remove(this.parentNode);
+                      })
 
+                      
                     
                     
                 })
